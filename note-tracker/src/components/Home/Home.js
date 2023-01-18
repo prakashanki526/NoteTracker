46c27d1b6220db414c739380be42e146bd295aa9
Ankit Prakash <prakashankit526@gmail.com>
2023-01-18 18:33:01 +0530

    create daily notes and bookmark toggle component

--- note-tracker/src/components/Home/Home.js
+++ note-tracker/src/components/Home/Home.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './Home.module.css';
 import Menubar from '../Menubar/Menubar';
 import NotesContainer from '../NotesContainer/NotesContainer';
 import arrow from '../../assets/arrow.svg';
 
 const Home = () => {
+    const [currentState, setCurrentState] = useState(0);
+
+    const [notes, setNotes] = useState(["abcd"]);
+    const [Bookmarks, setBookmarks] = useState(["avsh"]);
+
     return (
         <div className={styles.body}>
             <div className={styles.main}>
                 <div className={styles.left}>
-                    <Menubar />
+                    <Menubar currentState={currentState} setCurrentState={setCurrentState} />
                 </div>
                 <div className={styles.right}>
                     <div className={styles.upper}>
-                        <NotesContainer />
-                        <NotesContainer />
-                        <NotesContainer />
+                        <NotesContainer data={currentState === 0 ? notes : Bookmarks} />
                     </div>
                     <div className={styles.lower}>
                         <div className={styles.inputContainer}>
--- note-tracker/src/components/Menubar/Menubar.js
+++ note-tracker/src/components/Menubar/Menubar.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styles from './Menubar.module.css';
-import MenuElement from '../MenuElements/MenuElement.js';
 import titleicon from '../../assets/titleicon.svg';
+import notesIcon from '../../assets/notesIcon.svg';
+import bookmarkIcon from '../../assets/bookmarkIcon.svg';
 
-const Menubar = () => {
+const Menubar = (props) => {
+    const currentState = props.currentState;
+    const setCurrentState = props.setCurrentState;
+
     return (
         <div className={styles.menubar}>
             <div className={styles.title}>
                 <img src={titleicon} alt='loading'></img>
                 &nbsp; DAILY NOTE TRACKER
             </div>
-            <MenuElement name="Daily Notes" />
-            <MenuElement name="Bookmarks" />
+            <div className={currentState === 0 ? styles.containerSelected : styles.container} onClick={()=> setCurrentState(0)}>
+                <div className={styles.icon} style={{backgroundColor: "#806BFF"}}>
+                    <img src={notesIcon} alt=""></img>
+                </div>
+                <div className={styles.menu} style={{color : "#806BFF"}}>
+                    Daily Notes
+                </div>
+            </div>
+            <div className={currentState === 1 ? styles.containerSelected : styles.container} onClick={()=> setCurrentState(1)}>
+                <div className={styles.icon} style={{backgroundColor: "#FF6B6B"}}>
+                    <img src={bookmarkIcon} alt=""></img>
+                </div>
+                <div className={styles.menu} style={{color : "#FF6B6B"}}>
+                    Bookmarks
+                </div>
+            </div>
         </div>
     );
 };
